dc5f228bf7b214973d1459e62a6fa5bfa052dc07
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-11-02 21:42:54 -0700

    Adds dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
+use std::path::PathBuf;
 use axum::{routing::get, Router};
+use tower_http::services::ServeDir;
+use maud::{html, Markup, DOCTYPE};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+
+async fn hello_world() -> Markup {
+    html! {
+        (DOCTYPE)
+        html {
+            head {
+                title { "Hello, world!" }
+                link rel="stylesheet" href="/static/global.css";
+            }
+            body {
+                h1 { "Hello, world!" }
+                p { "Welcome to my website." }
+            }
+        }
+    }
 }
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(hello_world))
+        .nest_service("/static", ServeDir::new(PathBuf::from("static")))
+        ;
 
     Ok(router.into())
 }
